46d27ae7042429481343b692e822495be0087d6c
Luis Leyva <a01570367@itesm.mx>
2022-02-18 14:28:57 -0600

    Swapped Matches and MainMenu Code

--- src/components/mainMenu/MainMenu.jsx
+++ src/components/mainMenu/MainMenu.jsx
@@ -1,17 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import menuStyles from "../mainMenu/MainMenu.module.css";
 
+import { getMatches, getMatchesTBA, postUpdateMatches } from "../../api/apiCalls";
+
 function MainMenu() {
-    return (
-        <div className={ menuStyles.mainContainer }>
-            <h1>Menu Principal</h1>
-            <div className={ menuStyles.buttonContainer }>
-                {/* Add CSS and hrefs */ }
-                <a href="Teams">Teams Information</a>
-                <a href="Matches">Scout Matches</a>
+    const [matches, setMatches] = useState();
+
+    useEffect(() => {
+        async function getData() {
+            const data = await getMatches();
+            setMatches(data.count);
+        }
+        if (matches === undefined) {
+            getData();
+        }
+    }, [matches]);
+
+    // Filter TBA matches data
+    function filterMatchTeams(data) {
+        let tempData = data.filter(match => match.comp_level === "qm");
+        let filteredData = [];
+        tempData.forEach(match => {
+            filteredData.push({
+                comp_level: match.comp_level,
+                match_number: match.match_number,
+                alliances: {
+                    red_team_keys: match.alliances.red.team_keys,
+                    blue_team_keys: match.alliances.blue.team_keys,
+                }
+            })
+        });
+
+        filteredData.sort((a, b) => a.match_number - b.match_number);
+        return filteredData;
+    }
+
+    // Load and Refresh matches for the event
+    async function uploadData() {
+        setMatches(-1);
+        const data = await getMatchesTBA();
+        const number = data.filter(match => match.comp_level === "qm").length;
+        const filteredData = filterMatchTeams(data);
+        postUpdateMatches(filteredData);
+        setMatches(number);
+    }
+
+    async function downloadData() { }
+
+    if (matches === 0) {
+        return (
+            <div className={ menuStyles.mainContainer }>
+                <h1>Menu</h1>
+                <button onClick={ uploadData }>Load Regional Data</button>
             </div>
-        </div >
-    );
+        );
+    } else if (matches === -1) {
+        return (
+            <div className={ menuStyles.mainContainer }>
+                <h1>Menu</h1>
+                <p>Loading...</p>
+            </div>
+        );
+    } else {
+        return (
+            <div className={ menuStyles.mainContainer }>
+                <h1>Menu Principal</h1>
+                <div className={ menuStyles.buttonContainer }>
+                    {/* Add CSS and hrefs */ }
+                    <a href="Teams">Teams Information</a>
+                    <a href="Matches">Scout Matches</a>
+                </div>
+            </div >
+        );
+    }
 }
 
 export default MainMenu;
--- src/components/matches/Matches.jsx
+++ src/components/matches/Matches.jsx
@@ -1,35 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import matchesStyles from "../matches/Matches.module.css";
 
-import { keys } from "../../api/apiAuth";
-
-// Get matches for the event from TBA API
-async function getMatches() {
-    return fetch(`https://www.thebluealliance.com/api/v3/event/2020mxmo/matches/simple`, {
-        method: "GET",
-        headers: {
-            "Accept": "application/json",
-            "X-TBA-Auth-Key": keys,
-        },
-    }).then((data) => data.json());
-}
-
 function Matches() {
-    const [matches, setMatches] = useState();
-
-    useEffect(() => {
-        async function getData() {
-            const data = await getMatches();
-            console.log(data);
-        }
-        if (!matches) {
-            getData();
-        }
-    }, [matches]);
-
     return (
         <div className={ matchesStyles.mainContainer }>
-            <h1>Matches Prueba</h1>
+            <h1>Matches</h1>
         </div>
     );
 }
